5d1e3eb32bc191b3efdc4988a8cfa3605b877965
justinkim2015 <justinkim2015@gmail.com>
2022-06-03 11:08:20 +0900

    Finish 10

--- spec/10_drink_spec.rb
+++ spec/10_drink_spec.rb
@@ -10,6 +10,7 @@
 
 # If you are using VS Code, you can split the screen to see both files at
 # the same time (view menu -> editor layout).
+# rspec spec/10_drink_spec.rb
 
 require_relative '../lib/10_drink'
 
@@ -91,13 +92,16 @@ describe Drink do
     context 'when type is specified and ounces is default' do
       # Create an explicit subject, using 'described_class' and your choice of
       # beverage type.
+      subject(:beer) {described_class.new('beer')}
 
       # remove the 'x' before running this test
-      xit 'is your choice of beverage' do
+      it 'is your choice of beverage' do
+        expect(beer.type).to eq('beer')
       end
 
       # remove the 'x' before running this test
-      xit 'has 16 ounces' do
+      it 'has 16 ounces' do
+        expect(beer.ounces).to eq(16)
       end
     end
   end
@@ -106,18 +110,20 @@ describe Drink do
     context 'when drink has 16 ounces or more' do
       # Create an explicit subject, using 'described_class' and your choice of
       # beverage type.
-
+      subject(:soda) {described_class.new('soda')}
       # remove the 'x' before running this test
-      xit 'is full' do
+      it 'is full' do
+        expect(soda).to be_full
       end
     end
 
     context 'when drink has less than 16 ounces' do
       # Create an explicit subject, using 'described_class' and your choice of
       # beverage type. In addition, specify ounces to be any number under 16.
-
+      subject(:whiskey) {described_class.new('whiskey', 8)}
       # remove the 'x' before running this test
-      xit 'is not full' do
+      it 'is not full' do
+        expect(whiskey).not_to be_full
       end
     end
   end
