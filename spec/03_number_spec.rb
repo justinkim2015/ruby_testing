410dee8060c1100727ae676ec3426c37a43925e3
justinkim2015 <justinkim2015@gmail.com>
2022-06-03 10:17:54 +0900

    Finish 03

--- spec/03_number_spec.rb
+++ spec/03_number_spec.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+# rspec spec/03_number_spec.rb
 
 # In a typical workflow, the class being tested will be located in a separate
 # file from the tests. But for this simple example, let's keep everything together.
@@ -62,34 +63,35 @@ end
 describe Array do
   context 'when my_array has perfect values' do
     # Write a let variable 'my_array' that passes all tests.
+    let(:my_array) { [-3, 42, 102, 98] }
 
     # remove the 'x' before running this test
-    xit 'has a specific first value' do
+    it 'has a specific first value' do
       expect(my_array.first).to be_odd.and be <= -1
     end
 
     # remove the 'x' before running this test
-    xit 'has a specific last value' do
+    it 'has a specific last value' do
       expect(my_array.last).to be_even.and be < 99
     end
 
     # remove the 'x' before running this test
-    xit 'has a specific min value' do
+    it 'has a specific min value' do
       expect(my_array.min).not_to be < -9
     end
 
     # remove the 'x' before running this test
-    xit 'has a specific max value' do
+    it 'has a specific max value' do
       expect(my_array.max).to be > 100
     end
 
     # remove the 'x' before running this test
-    xit 'includes a value of 42' do
+    it 'includes a value of 42' do
       expect(my_array).to include(42)
     end
 
     # remove the 'x' before running this test
-    xit 'has a fourth element' do
+    it 'has a fourth element' do
       expect(my_array[3]).not_to be_nil
     end
   end
